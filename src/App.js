ae9a373ce68dae280c829bb7192afed97f5d6af7
shubh singh <101503723+Shubh484@users.noreply.github.com>
2023-04-11 18:57:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="flex-container">
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>April 8 2023</div>
+      <div className="expense-item-despriction"></div>
+      <h2>Car Insurance</h2>
+      <div className="expense-item-price">$294.67</div>
+    </div>
+  );
+}
+export default ExpenseItem;
